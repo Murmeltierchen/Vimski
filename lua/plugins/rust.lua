1257f1ea0dba60daa192130626c0303fbd365626
Murmeltierchen <ramon@schmaky.dev>
2026-05-06 21:56:24 +0200

    Fixed problem with rust

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -5,9 +5,7 @@ return {
         lazy = false,
         ft = { "rust" },
         config = function()
-            local mason_registry = require('mason-registry')
-            local codelldb = mason_registry.get_package("codelldb")
-            local extension_path = codelldb:get_install_path() .. "/extension/"
+            local extension_path = vim.fn.stdpath("data") .. "/mason/packages/codelldb/extension/"
             local codelldb_path = extension_path .. "adapter/codelldb"
             local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
 
@@ -15,8 +13,6 @@ return {
 
             vim.g.rustaceanvim = {
                 server = {
-                    on_attach = function(client, bufnr)
-                    end,
                     default_settings = {
                         ['rust-analyzer'] = {
                             cargo = {
