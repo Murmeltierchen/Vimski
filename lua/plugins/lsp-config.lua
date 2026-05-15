f5d10de86fa3b6dada1c5f7a596c897268944b68
Murmeltierchen <ramon@schmaky.dev>
2026-05-15 15:33:14 +0200

    main: Added themery and themes

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,54 +1,58 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        lazy = false,
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        lazy = false,
-        opts = {
-            auto_install = true,
-            ensure_installed = {
-                "delve",
-                "eslint_d",
-                "gofumpt",
-                "goimports-reviser",
-                "golangci-lint",
-                "gopls",
-                "html",
-                "lua_ls",
-                "prettierd",
-                "rust_analyzer",
-                "ts_ls",
-            },
-        },
-    },
-    {
-        "neovim/nvim-lspconfig",
-        lazy = false,
-        config = function()
-            local capabilities = require('blink.cmp').get_lsp_capabilities()
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		lazy = false,
+		opts = {
+			ensure_installed = {
+				"delve",
+				"eslint_d",
+				"gofumpt",
+				"goimports-reviser",
+				"golangci-lint",
+				"prettierd",
+				"stylua",
+			},
+		},
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+			ensure_installed = {
+				"gopls",
+				"html",
+				"lua_ls",
+				"ts_ls",
+			},
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-            vim.lsp.config("lua_ls", {
-                capabilities = capabilities,
-            })
-            vim.lsp.config("html", {
-                capabilities = capabilities,
-            })
-            vim.lsp.config("ts_ls", {
-                capabilities = capabilities,
-            })
+			vim.lsp.config("gopls", { capabilities = capabilities })
+			vim.lsp.config("html", { capabilities = capabilities })
+			vim.lsp.config("lua_ls", { capabilities = capabilities })
+			vim.lsp.config("ts_ls", { capabilities = capabilities })
 
-            vim.lsp.enable("lua_ls")
-            vim.lsp.enable("html")
-            vim.lsp.enable("ts_ls")
+			vim.lsp.enable("gopls")
+			vim.lsp.enable("html")
+			vim.lsp.enable("lua_ls")
+			vim.lsp.enable("ts_ls")
 
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "<leader>d", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "<leader>r", vim.lsp.buf.references, {})
-        end,
-    },
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>d", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>r", vim.lsp.buf.references, {})
+		end,
+	},
 }
