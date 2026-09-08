5dd14286f39a86c2cf0cab6f7005ceed2ceb5b68
Ramon <ramon@schmaky.dev>
2026-09-08 18:16:44 +0200

    main: Added commands to toggle formatting on save

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,28 +1,55 @@
 return {
-    "stevearc/conform.nvim",
-    opts = {
-        formatters_by_ft = {
-            css = { "prettierd" },
-            go = { "goimports-reviser", "gofumpt" },
-            graphql = { "prettierd" },
-            html = { "prettierd" },
-            javascript = { "prettierd" },
-            javascriptreact = { "prettierd" },
-            json = { "prettierd" },
-            jsonc = { "prettierd" },
-            less = { "prettierd" },
-            lua = { "stylua" },
-            markdown = { "prettierd" },
-            scss = { "prettierd" },
-            svelte = { "prettierd" },
-            typescript = { "prettierd" },
-            typescriptreact = { "prettierd" },
-            vue = { "prettierd" },
-            yaml = { "prettierd" },
-        },
-        format_on_save = {
-            timeout_ms = 500,
-            lsp_fallback = true,
-        }
-    }
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			css = { "prettierd" },
+			go = { "goimports-reviser", "gofumpt" },
+			graphql = { "prettierd" },
+			html = { "prettierd" },
+			javascript = { "prettierd" },
+			javascriptreact = { "prettierd" },
+			json = { "prettierd" },
+			jsonc = { "prettierd" },
+			less = { "prettierd" },
+			lua = { "stylua" },
+			markdown = { "prettierd" },
+			scss = { "prettierd" },
+			svelte = { "prettierd" },
+			typescript = { "prettierd" },
+			typescriptreact = { "prettierd" },
+			vue = { "prettierd" },
+			yaml = { "prettierd" },
+		},
+		format_on_save = function(bufnr)
+			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+				return
+			end
+
+			return {
+				timeout_ms = 500,
+				lsp_fallback = true,
+			}
+		end,
+	},
+	config = function(_, opts)
+		require("conform").setup(opts)
+
+		vim.api.nvim_create_user_command("FormatDisable", function(args)
+			if args.bang then
+				vim.g.disable_autoformat = true
+			else
+				vim.b.disable_autoformat = true
+			end
+		end, {
+			desc = "Deaktiviere Autoformat on Save",
+			bang = true,
+		})
+
+		vim.api.nvim_create_user_command("FormatEnable", function()
+			vim.b.disable_autoformat = false
+			vim.g.disable_autoformat = false
+		end, {
+			desc = "Reaktiviere Autoformat on Save",
+		})
+	end,
 }
